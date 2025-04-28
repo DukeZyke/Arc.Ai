c46ab87fdbb4d2f7a6796ed92d01a168350d4dd3
DukeZyke <zurielcalix414141@gmail.com>
2025-04-29 07:42:32 +0800

    Update urls.py
    added path for login and signup

--- Arc_Ai/Arc_Ai/core/urls.py
+++ Arc_Ai/Arc_Ai/core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('organization/', views.organization, name='organization'),
     path('saved/', views.saved, name='saved'),
     path('email/', views.email, name='email'),
