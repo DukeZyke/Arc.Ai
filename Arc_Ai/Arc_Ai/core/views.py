f8ee7d67e4dc5a3a14c9734a2c970bbc2751ea00
DukeZyke <zurielcalix414141@gmail.com>
2025-04-29 07:44:46 +0800

    Update views.py

--- Arc_Ai/Arc_Ai/core/views.py
+++ Arc_Ai/Arc_Ai/core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+def login(request):
+    return render(request, 'core/login.html')
+
+def signup(request):
+    return render(request, 'core/signup.html')
+
 def home(request):
     return render(request, 'core/home.html')
 
