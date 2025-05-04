7dfa7ab8d381c2d2fed7748c2da52955954c29aa
GinnoLmao <ginnoarostique2004@gmail.com>
2025-05-05 02:16:51 +0800

    Saving Changes

--- Arc_Ai/Arc_Ai/core/urls.py
+++ Arc_Ai/Arc_Ai/core/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.organization, name='organization'),
+    path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('saved/', views.saved, name='saved'),
     path('email/', views.email, name='email'),
+    path('home/', views.home, name='home'),
 ]
