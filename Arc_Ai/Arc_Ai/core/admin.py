6fe55447bd77f496e71509b929b07be7b8bd5399
mawii0 <jmscordero1232@gmail.com>
2025-05-05 02:25:23 +0800

    Hi

--- Arc_Ai/Arc_Ai/core/admin.py
+++ Arc_Ai/Arc_Ai/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, PersonalInformation, EmployeeAward
 
 # Register your models here.
+
+# Profile page
+admin.site.register(Project)
+admin.site.register(PersonalInformation)
+admin.site.register(EmployeeAward)
--- Arc_Ai/Arc_Ai/core/urls.py
+++ Arc_Ai/Arc_Ai/core/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('saved/', views.saved, name='saved'),
     path('email/', views.email, name='email'),
+    path('home/', views.home, name='home'),
     path('organization/', views.organization, name='organization'),
+    path('profilepage/', views.profilepage, name='profilepage'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
