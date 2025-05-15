6f721943d394f154910b9df1181c0d5bba70f348
mawii0 <jmscordero1232@gmail.com>
2025-05-15 08:25:01 +0800

    pull

--- Arc_Ai/Arc_Ai/core/urls.py
+++ Arc_Ai/Arc_Ai/core/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('saved/', views.saved, name='saved'),
     path('email/', views.email, name='email'),
-    path('admin_user_page/', views.admin_project_page, name='admin_user_page'),
+    path('admin_project_page/', views.admin_project_page, name='admin_project_page'),
     path('home/', views.home, name='home'),
     path('user_involved_map/', views.user_involved_map, name='user_involved_map'),
     path('organization/', views.organization, name='organization'),
@@ -28,7 +28,12 @@ urlpatterns = [
     path('move-files-to-trash/', views.move_files_to_trash, name='move_files_to_trash'),
     path('empty-trash/', views.empty_trash, name='empty_trash'),
     path('api/notifications/', views.get_notifications, name='get_notifications'),
-    path('edit_user_profile', views.edit_user_profile, name='edit_user_profile'),
+    path('edit_user_profile/', views.edit_user_profile, name='edit_user_profile'),
+
+    # FOR EDITING PROJECTS
+    path('core/project/<int:project_id>/edit/', views.aa, name='aa'),
+    # FOR DELETING PROJECTS
+    path('core/project/<int:project_id>/delete/', views.delete_project, name='delete_project'),
 
 
     # PRACTICE TEMPLATES
