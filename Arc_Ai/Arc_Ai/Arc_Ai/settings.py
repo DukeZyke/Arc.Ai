7599dc30c2ca98a0250a539d8e3679b20a818d65
mawii0 <jmscordero1232@gmail.com>
2025-05-23 21:56:25 +0800

    settings, data.json

--- Arc_Ai/Arc_Ai/Arc_Ai/settings.py
+++ Arc_Ai/Arc_Ai/Arc_Ai/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'Arc_Ai.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'arc.ai_database',
+        'USER': 'test_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
